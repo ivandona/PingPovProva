b84331bd8c68e5184a4e650bfcc1732c19054234
ivan <ivan.dona@studenti.unitn.it>
2022-05-23 11:29:45 +0200

    aggiustamenti indentazione

--- api/tornei.js
+++ api/tornei.js
@@ -23,111 +23,111 @@ module.exports = function (app, mongoose) {
         })
     })
 
-app.get('/v1/tornei/creaTorneo', (req, res) => {
-    res.render("pages/crea_torneo");
-});
+    app.get('/v1/tornei/creaTorneo', (req, res) => {
+        res.render("pages/crea_torneo");
+    });
 
 
 
-app.post('/v1/tornei/creaTorneo',(req, res) => {
-    console.log(req.session.user)
-    const nuovo_Torneo = new Torneo({
-        nome_torneo: req.body.torneo.nome_torneo,
-        data: req.body.torneo.data,
-        organizzatore: req.session.user,
-        sede: req.body.torneo.sede,
-        max_partecipanti: req.body.torneo.numero_partecipanti,
-        numero_partecipanti: 0,
-    })
-    if (req.body.torneo.admin_gioca == true){
-        nuovo_Torneo.giocatori.push(req.session.user);
-        nuovo_Torneo.numero_partecipanti =1
-    }
-    
-    if (nuovo_Torneo.organizzatore == "" || nuovo_Torneo.organizzatore == undefined){
-        res.send("Errore, login non eseguito");
-        return;
-    }
-    nuovo_Torneo.save().then(() => console.log('Torneo salvato'));
-    console.log(nuovo_Torneo);
-    res.send(Torneo.nuovo_Torneo);
-});
-
-
-
-app.post('/v1/iscrivi/:id' , async function(req,res){
-
-    const nome_utente=req.body.username; 
-    const id= req.params.id;
-    try{
-        res.json(await Torneo.findById(id).update({ $push: { giocatori: nome_utente } }));
-        }  catch (err) {
-            console.error(`Error while updating programming language`, err.message);
-            next(err);
-          }
-       
-
-});
-app.post('/v1/disiscrivi/:id' , async function(req,res){
-
-    const nome_utente=req.body.username; 
-    const id= req.params.id;
-    try{
-        res.json(await Torneo.findById(id).update({ $pull: { giocatori: nome_utente } }));
-        }  catch (err) {
-            console.error(`Error while updating programming language`, err.message);
-            next(err);
-          }
-       
-
-});
-
-
-
-//GET ONE BOOK
-app.get('/v1/torneo/:id', (req, res) => {
-    const id = req.params.id;
-    Torneo.find({ "_id": id }).lean().exec((err, torneo)=> { res.render('pages/torneo',{torneo:JSON.stringify(torneo)}) })
-});
-
-//DELETE
-app.delete('/v1/tornei/:id', (req, res) => {
-    
-    const id = req.params.id;
-    Torneo.find({ "_id": id }.lean(), function (err, docs) {
-        if (docs.organizzatore == req.session.username) {
-            Torneo.findByIdAndRemove(id, function (err, docs) {
-                if (err) {
-                    res.send('Torneo non trovato')
-                } else {
-                    res.send('Torneo correttamente cancellato')
-                }
-            });
-        } else {
-            res.send("Non sei tu l'organizzatore")
+    app.post('/v1/tornei/creaTorneo',(req, res) => {
+        console.log(req.session.user)
+        const nuovo_Torneo = new Torneo({
+            nome_torneo: req.body.torneo.nome_torneo,
+            data: req.body.torneo.data,
+            organizzatore: req.session.user,
+            sede: req.body.torneo.sede,
+            max_partecipanti: req.body.torneo.numero_partecipanti,
+            numero_partecipanti: 0,
+        })
+        if (req.body.torneo.admin_gioca == true){
+            nuovo_Torneo.giocatori.push(req.session.user);
+            nuovo_Torneo.numero_partecipanti =1
         }
-    })
-
-})
-
-app.put('/v1/torneo/:id', (req, res) => {
-    const id = req.params.id;
-    Torneo.find({ "_id": id }, function (err, docs) {
-        if (docs.organizzatore == req.session.username) {
-            Torneo.findByIdAndUpdate(id, function (err, docs) {
-                if (err) {
-                    res.send('Torneo non trovato')
-                } else {
-                    res.send('Torneo correttamente cancellato')
-                }
-            });
-        } else {
-            res.send("Non sei tu l'organizzatore")
+        
+        if (nuovo_Torneo.organizzatore == "" || nuovo_Torneo.organizzatore == undefined){
+            res.send("Errore, login non eseguito");
+            return;
         }
-    })
-})
-app.get('/v1/torneo', (req, res) => {
+        nuovo_Torneo.save().then(() => console.log('Torneo salvato'));
+        console.log(nuovo_Torneo);
+        res.send(Torneo.nuovo_Torneo);
+    });
 
-    res.render("pages/torneo")
-})
+
+
+    app.post('/v1/iscrivi/:id' , async function(req,res){
+
+        const nome_utente=req.body.username; 
+        const id= req.params.id;
+        try{
+            res.json(await Torneo.findById(id).update({ $push: { giocatori: nome_utente } }));
+            }  catch (err) {
+                console.error(`Error while updating programming language`, err.message);
+                next(err);
+            }
+        
+
+    });
+    app.post('/v1/disiscrivi/:id' , async function(req,res){
+
+        const nome_utente=req.body.username; 
+        const id= req.params.id;
+        try{
+            res.json(await Torneo.findById(id).update({ $pull: { giocatori: nome_utente } }));
+            }  catch (err) {
+                console.error(`Error while updating programming language`, err.message);
+                next(err);
+            }
+        
+
+    });
+
+
+
+    //GET ONE BOOK
+    app.get('/v1/torneo/:id', (req, res) => {
+        const id = req.params.id;
+        Torneo.find({ "_id": id }).lean().exec((err, torneo)=> { res.render('pages/torneo',{torneo:JSON.stringify(torneo)}) })
+    });
+
+    //DELETE
+    app.delete('/v1/tornei/:id', (req, res) => {
+        
+        const id = req.params.id;
+        Torneo.find({ "_id": id }.lean(), function (err, docs) {
+            if (docs.organizzatore == req.session.username) {
+                Torneo.findByIdAndRemove(id, function (err, docs) {
+                    if (err) {
+                        res.send('Torneo non trovato')
+                    } else {
+                        res.send('Torneo correttamente cancellato')
+                    }
+                });
+            } else {
+                res.send("Non sei tu l'organizzatore")
+            }
+        })
+
+    })
+
+    app.put('/v1/torneo/:id', (req, res) => {
+        const id = req.params.id;
+        Torneo.find({ "_id": id }, function (err, docs) {
+            if (docs.organizzatore == req.session.username) {
+                Torneo.findByIdAndUpdate(id, function (err, docs) {
+                    if (err) {
+                        res.send('Torneo non trovato')
+                    } else {
+                        res.send('Torneo correttamente cancellato')
+                    }
+                });
+            } else {
+                res.send("Non sei tu l'organizzatore")
+            }
+        })
+    })
+    app.get('/v1/torneo', (req, res) => {
+
+        res.render("pages/torneo")
+    })
 }
