8f8406a400c51c414c731ba0b279fc5969bcfb55
Davide Nardi <davide.nardi-1@studenti.unitn.it>
2022-06-07 16:42:15 +0200

    test deploy

--- config.ts
+++
@@ -1,3 +0,0 @@
-export const GOOGLE_CLIENT_ID ='71410486894-clfqt9gadnobb4j27vn4sk36dbh0l5di.apps.googleusercontent.com';
-export const GOOGLE_CLIENT_SECRET ="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";
-'mongodb+srv://Hydran00:aaa@cluster0.5c9ts.mongodb.net/test'
